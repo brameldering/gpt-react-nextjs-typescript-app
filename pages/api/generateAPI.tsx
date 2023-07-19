e72703f4b7b99da7d296d25cadbb4b3a5fd8c0b7
brameldering <bram.eldering@gmail.com>
2023-07-19 19:05:03 +0200

    Added error handling for axios request

--- pages/api/generateAPI.tsx
+++ pages/api/generateAPI.tsx
@@ -3,18 +3,26 @@ import axios from "axios";
 const API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 
 const callGPT_API = async (name: string): Promise<string> => {
-  const response = await axios.post(
-    "https://api.openai.com/v1/completions",
-    {
-      prompt: "Give me 3 possible pet names for " + name,
-      model: "text-davinci-003",
-      temperature: 0.8,
-      max_tokens: 100,
-    },
-    { headers: { "Content-Type": "application/json", Authorization: `Bearer ${API_KEY}` } }
-  );
-  //   console.log(response.data.choices[0]);
-  return response.data.choices[0].text;
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        prompt: "Give me 3 possible pet names for " + name,
+        model: "text-davinci-003",
+        temperature: 0.8,
+        max_tokens: 100,
+      },
+      { headers: { "Content-Type": "application/json", Authorization: `Bearer ${API_KEY}` } }
+    );
+    console.log(response);
+    return response.data.choices[0].text;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      return "An Axios error occurred " + error.message;
+    } else {
+      return "An error occurred";
+    }
+  }
 };
 
 export default callGPT_API;
