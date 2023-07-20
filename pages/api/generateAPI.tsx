95d4d1d5789abca7b3696a301c31f1824f7a3bc5
brameldering <bram.eldering@gmail.com>
2023-07-20 13:00:07 +0200

    Enhanced logic to generate one word and multi word names. Show names in cards.  Processed string result from openAI to array of names.  Added loading spinner.

--- pages/api/generateAPI.tsx
+++ pages/api/generateAPI.tsx
@@ -2,19 +2,18 @@ import axios from "axios";
 
 const API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
 
-const callGPT_API = async (name: string): Promise<string> => {
+const callGPT_API = async (prompt: string): Promise<string> => {
   try {
     const response = await axios.post(
       "https://api.openai.com/v1/completions",
       {
-        prompt: "Give me 3 possible pet names for " + name,
+        prompt: prompt,
         model: "text-davinci-003",
         temperature: 0.8,
         max_tokens: 100,
       },
       { headers: { "Content-Type": "application/json", Authorization: `Bearer ${API_KEY}` } }
     );
-    console.log(response);
     return response.data.choices[0].text;
   } catch (error) {
     if (axios.isAxiosError(error)) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,108 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { CircularProgress } from "@mui/material";
 import callGPT_API from "./api/generateAPI";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [generatedNames, setGeneratedNames] = useState("");
-  const [isFetching, setIsFetching] = useState(false);
+  const [animalInput, setAnimalInput] = useState<string>("");
+  const [generatedNames, setGeneratedNames] = useState<string[] | null>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-  const handleAnimalInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleAnimalInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setAnimalInput(e.target.value);
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const removeReturns = (inputString: string): string => {
+    // Use regular expression to remove carriage returns
+    const result = inputString.replace(/\r\n/g, " ").replace(/[\r\n]/g, " ");
+    return result;
+  };
+
+  const extractArrayFromWordsBetweenDigitsAndDots = (inputString: string): string[] | null => {
+    const regex = /\b\d+\.\s+(.*?)\s*(?=\b\d+\.\s*|$)/g;
+    const matches = inputString.match(regex);
+    if (!matches) {
+      return [];
+    }
+    return matches.map((match) => match.replace(/\d+\.\s+/, "").trim());
+  };
+
+  // parse generated names string to an array, removing the prefixed numbers (1. ,2. , ...,5.)
+  const parseGeneratedNamesToArray = (strNames: string): string[] => {
+    // remove carriage returns from strNames
+    const strWithoutReturns = removeReturns(strNames);
+    let arrayOfNames = extractArrayFromWordsBetweenDigitsAndDots(strWithoutReturns);
+    if (!arrayOfNames) {
+      arrayOfNames = [];
+    }
+    return arrayOfNames;
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<any> => {
     e.preventDefault();
+    setErrorMessage("");
+    setGeneratedNames([]);
+    if (animalInput.trim() === "") {
+      setErrorMessage("Please provide a pet description");
+      setAnimalInput("");
+      return;
+    }
     setIsFetching(true);
-    await callGPT_API(animalInput);
-    const nameChoices = await callGPT_API(animalInput);
-    setGeneratedNames(nameChoices);
+    // get 5 single word names
+    let prompt = "Give me five possible pet names consisting of one word for " + animalInput;
+    const stringOfOneWordNames = await callGPT_API(prompt);
+    const arrayOfOneWordNames = parseGeneratedNamesToArray(stringOfOneWordNames);
+    // get 5 multi-word names
+    prompt =
+      "Give me five possible pet names consisting of a combination of multiple words for " +
+      animalInput;
+    const stringOfMultiWordNames = await callGPT_API(prompt);
+    // const stringOfMultiWordNames =
+    // "1. Mischievous Mittens in da house with funky paws 2. Prankster Patches 3. Frisky Fluffy 4. Plucky Paws 5. Playful Purrball";
+    const arrayOfMultiWordNames = parseGeneratedNamesToArray(stringOfMultiWordNames);
+    let arrayOfNames: string[];
+    // combine one word and multi word names
+    arrayOfNames = arrayOfOneWordNames.concat(arrayOfMultiWordNames);
+    setGeneratedNames(arrayOfNames);
     setIsFetching(false);
     setAnimalInput("");
   };
 
   return (
-    <>
-      <div>
-        <Head>
-          <title>Generate pet name</title>
-          <meta name='description' content='Generate a pet name' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
-        <main className={styles.main}>
-          <h3>Name my Pet</h3>
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              name='animal'
-              placeholder='Enter an animal'
-              value={animalInput}
-              onChange={handleAnimalInputChange}
-            />
-            <button type='submit' name='generate'>
-              Generate
-            </button>
-            <div className={styles.result}>
-              {isFetching && <h3>Generating...</h3>}
-              {!isFetching && generatedNames && <h3>Generated Names:</h3>}
-              <p>{generatedNames}</p>
-            </div>
-          </form>
-        </main>
-      </div>
-    </>
+    <div className={styles.body}>
+      <Head>
+        <title>Generate pet name</title>
+        <meta name='description' content='Generate a pet name' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={styles.main}>
+        <h3>Name my Pet</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='animal'
+            placeholder='Describe your pet here'
+            value={animalInput}
+            onChange={handleAnimalInputChange}
+          />
+          <button type='submit' name='generate'>
+            Generate
+          </button>
+        </form>
+        <div className={styles.result}>
+          {errorMessage && <h4 className={styles.error}>{errorMessage}</h4>}
+          {isFetching && !errorMessage && (
+            <h4>
+              <CircularProgress />
+            </h4>
+          )}
+          {!isFetching && generatedNames && !errorMessage && <h4>Generated Names:</h4>}
+          <ul>{generatedNames && generatedNames.map((item) => <li key={item}>{item}</li>)}</ul>
+        </div>
+      </main>
+    </div>
   );
 }
